59adf769d0c3680c973e2da57f2c87f812c7533f
Tina Coppedge <tdcoppedge1@buffs.wtamu.edu>
2025-05-12 19:14:12 -0500

    more debugging for cart icon

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -282,15 +282,19 @@ function ProductList({ onHomeClick }) {
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
                     <div style={{ position: 'relative' }}>
-                        <a href="#" onClick={(e) => handleCartClick(e)} style={styleA} className="cart-icon-link">
-                            <img
-                                src="https://www.svgrepo.com/show/528106/shopping-bag-alt.svg"
-                                alt="Cart"
-                                className="cart-icon"
-                                style={{ width: '40px', height: '40px' }}
-                            />
+                        <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+                            <h1 className="cart">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" height="68" width="68">
+                                <rect width="256" height="256" fill="none"></rect>
+                                <circle cx="80" cy="216" r="12" fill="white" />
+                                <circle cx="184" cy="216" r="12" fill="white" />
+                                <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8"
+                                fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2">
+                                </path>
+                            </svg>
+                            </h1>
                             {totalItems > 0 && (
-                                <span className="cart_quantity_count">{totalItems}</span>
+                            <span className="cart_quantity_count">{totalItems}</span>
                             )}
                         </a>
                     </div>
