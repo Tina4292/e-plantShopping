c2ad72a68725da2ed0862b769b2250226dbb468c
Tina Coppedge <tdcoppedge1@buffs.wtamu.edu>
2025-05-12 19:31:51 -0500

    updating cart item info and cart item image size

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -56,6 +56,7 @@ const CartItem = ({ onContinueShopping }) => {
             <img className="cart-item-image" src={item.image} alt={item.name} />
             <div className="cart-item-details">
               <div className="cart-item-name">{item.name}</div>
+              <div className="cart-item-description">{item.description}</div>
               <div className="cart-item-cost">{item.cost}</div>
               <div className="cart-item-quantity">
                 <button className="cart-item-button cart-item-button-dec" onClick={() => handleDecrement(item)}>-</button>
